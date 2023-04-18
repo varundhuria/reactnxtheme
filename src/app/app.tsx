d6d59ee201d99409d94d023a5bce4c5082d5dfb0
varundhuria <varun.dhuria@gmail.com>
2023-04-18 18:58:18 +0530

    Initial commit

---
+++ src/app/app.tsx
@@ -0,0 +1,23 @@
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+
+import NxWelcome from "./nx-welcome";
+
+
+
+export function App() {
+
+  return (
+    <>
+      
+      <NxWelcome title="reactnxtheme"/>
+     
+      <div />
+    </>);
+
+}
+
+
+export default App;
+
